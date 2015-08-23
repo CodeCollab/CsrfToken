2931c69149f1c283693aeb3f7ed5b35e40d5b1a0
PeeHaa <info@pieterhordijk.com>
2015-08-23 16:47:58 +0200

    Added test bootstrap

---
+++ test/bootstrap.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+/**
+ * Bootstrap the tests
+ *
+ * PHP version 7.0
+ *
+ * @category   CodeCollabTest
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace CodeCollabTest;
+
+/**
+ * Load PHPUnit
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Setup data directory
+ */
+define('TEST_DATA_DIR', __DIR__ . '/Data');
