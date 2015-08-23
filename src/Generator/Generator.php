82274d6e8d0e89f266727e8a7ba788dbb134ff2e
PeeHaa <info@pieterhordijk.com>
2015-08-23 16:41:22 +0200

    Added generator interface

---
+++ src/Generator/Generator.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+/**
+ * CSRF token generator interface
+ *
+ * PHP version 7.0
+ *
+ * @category   CodeCollab
+ * @package    CsrfToken
+ * @package    Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace CodeCollab\CsrfToken\Generator;
+
+/**
+ * CSRF token generator interface
+ *
+ * @category   CodeCollab
+ * @package    CsrfToken
+ * @package    Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+interface Generator
+{
+    /**
+     * Generates a token
+     *
+     * @todo Check whether internals can actually make a sane decision on handling errors
+     *
+     * @return string The generated token
+     *
+     * @throws \CodeCollabLib\CsrfToken\InsufficientStrengthException When a token with sufficient strength
+     *                                                                could not be generated
+     */
+    public function generate(): string;
+}
