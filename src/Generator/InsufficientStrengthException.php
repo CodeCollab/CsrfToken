4940fd30287d5eb387d92e2091cecb5a994da79c
PeeHaa <info@pieterhordijk.com>
2015-08-23 16:45:08 +0200

    Added exception for when a generator cannot generator a sufficientely strong token

---
+++ src/Generator/InsufficientStrengthException.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+/**
+ * Exception which gets thrown when an insufficient strong key is being generated
+ *
+ * PHP version 7.0
+ *
+ * @category   CodeCollab
+ * @package    CsrfToken
+ * @subpackage Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace CodeCollab\Security\Encryption;
+
+/**
+ * Exception which gets thrown when an insufficient strong key is being generated
+ *
+ * @category   CodeCollab
+ * @package    CsrfToken
+ * @subpackage Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InsufficientStrengthException extends \Exception
+{
+}
